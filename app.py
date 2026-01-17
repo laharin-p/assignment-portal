cb2847442eea2cffe8a1801c77074b329eb1c4e4
Pathan.Laharin <plaharin6@gmail.com>
2026-01-17 21:32:21 +0530

    fix

--- app.py
+++ app.py
@@ -14,8 +14,19 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "dev-secret")
 
 # ---------------- DATABASE ----------------
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///assignment.db"
+# ---------------- DATABASE ----------------
+database_url = os.environ.get("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL not set")
+
+# Fix Render / Heroku postgres:// issue
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
 # ---------------- FOLDERS ----------------
