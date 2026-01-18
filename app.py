4ed48ec8a949f52b9b612387485917a89bae380f
Pathan.Laharin <plaharin6@gmail.com>
2026-01-18 08:34:00 +0530

    connect

--- app.py
+++ app.py
@@ -337,5 +337,5 @@ def download_submission(filename):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        
+        app.run(debug=True)
         print("✅ DATABASE READY")
